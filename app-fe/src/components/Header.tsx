ef0eeb3ceb65adeecb5abed99e11081d9e6f3aa3
ntdHelen <nguyenthudiu03122003@gmail.com>
2024-05-27 11:55:38 +0700

    update

--- app-fe/src/components/Header.tsx
+++ app-fe/src/components/Header.tsx
@@ -1,8 +1,58 @@
+import React from "react";
+import { Button, Container, Dropdown, DropdownToggle, Nav, NavLink, Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from "react-bootstrap";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { MdNotifications } from "react-icons/md";
+
 const Header = () => {
     return (
-        <div>
-            <h1>This is a header</h1>
-        </div>
+        <Navbar
+            expand="lg"
+            style={{ backgroundColor: "#242034" }}
+        >
+            <Container>
+                <NavbarBrand href="" style={{ color: "#ED154C" }}>
+                    PLAYPAL
+                </NavbarBrand>
+                <NavbarToggle aria-controls="basic-navbar-nav" />
+                <NavbarCollapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavLink href="" style={{ color: "white" }}>Home</NavLink>
+                        <NavLink href="">About us</NavLink>
+                        <NavLink href="">Blog</NavLink>
+                    </Nav>
+
+                    <Nav>
+                        <NavLink className="me-2" href="#">
+                            <Button>
+                                Login
+                            </Button>
+                        </NavLink>
+                        <NavLink href="#">
+                            <Button>
+                                Register
+                            </Button>
+                        </NavLink>
+
+                    </Nav>
+
+
+
+                    {/* <Nav>
+                        <Dropdown>
+                            <MdNotifications />
+                            <DropdownToggle
+                                variant="none"
+                                id="dropdown-basic"
+                                style={{ color: "white" }}>
+                                <BsPersonCircle />
+                            </DropdownToggle>
+
+                        </Dropdown>
+                    </Nav> */}
+                </NavbarCollapse>
+            </Container>
+        </Navbar>
     );
 };
 
