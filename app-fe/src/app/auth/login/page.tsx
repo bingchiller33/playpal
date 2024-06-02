8ec39b11d2cf2ac597c8856225992e6c0a6c78aa
MeoBeoSiTInh <daocuong.work@gmail.com>
2024-06-02 15:05:18 +0700

    login with google and login frontend

--- app-fe/src/app/auth/login/page.tsx
+++ app-fe/src/app/auth/login/page.tsx
@@ -16,36 +16,47 @@ export default async function Login() {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const response = await signIn('credentials', {
-      email: formData.get('email') as string,
-      password: formData.get('password') as string,
+    const response = await signIn("credentials", {
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
       redirect: false,
     });
-    
-    
+
     console.log({ response });
     if (!response?.error) {
       console.log("vailon success");
+      router.push("/auth/testAuth");
     }
+  };
+  const handleGoogleSignIn = async () => {
+    await signIn("google", { callbackUrl: "/auth/testAuth" });
   };
 
   return (
     <main>
       <Header />
-      <Container>
-        <Row className="justify-content-center">
+      <Container fluid
+        style={{
+          backgroundImage: 'url("https://scontent.fhan3-2.fna.fbcdn.net/v/t1.15752-9/436421699_1612300562942126_2734689648388013401_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFGhc1C5OPqArCOal9mBDFfcVbI3Nr8HopxVsjc2vweimdphGNDEw601zbnCk9IN88HfSUkECClTkoMvCcPNm6l&_nc_ohc=2VqeA9lMY0MQ7kNvgGM8OSX&_nc_ht=scontent.fhan3-2.fna&oh=03_Q7cD1QF1l9oqQ0z0zLLZgL6SM3YCZZk_O3I1hW3E32cwDxs4eg&oe=668379E8")', // Replace with your image URL
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <Row className="justify-content-center" style={{paddingTop:"50px", paddingBottom:"150px"}}>
           <Col xs={12} md={8} lg={4}>
             <div className={styles["form-container"]}>
               <h2 className={styles["title"]}>Welcome back!</h2>
-              <p className="">
+              <p style={{ fontSize: "0.9rem" }} className="">
                 Don't have an account?{" "}
-                <a href="/auth/register">
+                <a href="/auth/register" style={{ textDecoration: "none" }}>
                   <span style={{ color: "#ED154C" }}>
                     Create a new account now
                   </span>
                 </a>
               </p>
-              <p className="">It's FREE! Take less than a minute</p>
+              <p style={{ fontSize: "0.9rem" }} className="">
+                It's FREE! Take less than a minute
+              </p>
               <Form onSubmit={handleSubmit}>
                 <Form.Group
                   controlId="formEmail"
@@ -76,14 +87,25 @@ export default async function Login() {
                 <Button variant="danger" type="submit" className="w-100 mt-1">
                   Login
                 </Button>
-                <Button className={`${styles["login_with_google"]} w-100 mt-2`}>
+                <Button
+                  className={`${styles["login_with_google"]} w-100 mt-2`}
+                  onClick={handleGoogleSignIn}
+                >
                   <img
-                    src="" // Update this path to your logo location
+                    src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" // Update this path to your logo location
                     alt="Google logo"
                     className={styles["google-logo"]}
                   />
                   Login with Google
                 </Button>
+                <p style={{ fontSize: "0.9rem", textAlign: "center"}} className="mt-3">
+                  Forget Password?{" "}
+                  <a href="/auth/register" style={{ textDecoration: "none" }}>
+                    <span style={{ color: "#ED154C" }}>
+                      Click here
+                    </span>
+                  </a>
+                </p>
               </Form>
             </div>
           </Col>
