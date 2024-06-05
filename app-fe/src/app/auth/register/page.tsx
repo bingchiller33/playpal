a597639e1d9fd8539bac2c83a40bb57f6746e3ff
MeoBeoSiTInh <daocuong.work@gmail.com>
2024-06-05 16:13:01 +0700

    update background image

--- app-fe/src/app/auth/register/page.tsx
+++ app-fe/src/app/auth/register/page.tsx
@@ -41,7 +41,7 @@ export default async function Register() {
         fluid
         style={{
           backgroundImage:
-            'url("https://scontent.fhan3-2.fna.fbcdn.net/v/t1.15752-9/436421699_1612300562942126_2734689648388013401_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFGhc1C5OPqArCOal9mBDFfcVbI3Nr8HopxVsjc2vweimdphGNDEw601zbnCk9IN88HfSUkECClTkoMvCcPNm6l&_nc_ohc=2VqeA9lMY0MQ7kNvgGM8OSX&_nc_ht=scontent.fhan3-2.fna&oh=03_Q7cD1QF1l9oqQ0z0zLLZgL6SM3YCZZk_O3I1hW3E32cwDxs4eg&oe=668379E8")', // Replace with your image URL
+            'url("/assets/images/Moodboard.png")', // Replace with your image URL
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
