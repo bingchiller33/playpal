357ed53b0962e57680e3344dfdcfee82371bd158
tachibanayui <33594017+tachibanayui@users.noreply.github.com>
2024-06-05 11:49:16 +0700

    add tests

--- app-fe/src/utils/index.ts
+++ app-fe/src/utils/index.ts
@@ -3,6 +3,10 @@ export function jsonStrip<T>(a: T): T {
 }
 
 export function minMap(input: Record<string, number>) {
+    if (!Object.keys(input).length) {
+        return undefined;
+    }
+
     return Object.keys(input)
         .map((x) => input[x])
         .reduce((a, b) => Math.min(a, b));
