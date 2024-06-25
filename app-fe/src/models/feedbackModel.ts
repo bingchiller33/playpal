328625b966cf6bda2de432377c99f367c18be8f6
MeoBeoSiTInh <daocuong.work@gmail.com>
2024-06-25 13:42:27 +0700

    create model

---
+++ app-fe/src/models/feedbackModel.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema, Model } from "mongoose";
+import { WithId } from "@/utils/types";
+import { IAccount } from "./account";
+import { Mode } from "fs";
+
+
+export interface IFeedback {
+    giver_id: WithId<IAccount>;
+    sender_id: WithId<IAccount>;
+    rate: number;
+    text: string;
+    vote: number;
+}
+
+const feedbackSchema = new Schema<IFeedback>(
+    {
+        giver_id: {
+            type: mongoose.Types.ObjectId,
+            require: true,
+        },
+        sender_id: {
+            type: mongoose.Types.ObjectId,
+            require: true,
+        },
+        rate: {
+            type: Number,
+            require: true,
+        },
+        text: {
+            type: String,
+            require: true,
+        },
+        vote: {
+            type: Number,
+            require: true,
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Feedback: Model<IFeedback> = mongoose.models.feedbacks || mongoose.model<IFeedback>("feedbacks", feedbackSchema)
+
+export default Feedback;
