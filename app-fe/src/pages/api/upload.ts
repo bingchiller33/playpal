776623cc3de42b5bc4f6c0d47b33a0d8e79b2429
ntdHelen <nguyenthudiu03122003@gmail.com>
2024-07-10 16:03:38 +0700

    update env cloudinary

--- app-fe/src/pages/api/upload.ts
+++ app-fe/src/pages/api/upload.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import cloudinary from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
